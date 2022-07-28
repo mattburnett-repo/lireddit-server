1bdaf16be26f27fbbac8dd483c27092d542b0020
Matt Burnett <mburnett@tutanota.de>
2022-07-28 15:57:13 +0200

    URQL Pagination Start ending 6:50:02

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,8 +1,9 @@
 
 import { isAuth } from '../middleware/isAuth'
 import { MyContext } from 'src/types'
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql'
+import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver, UseMiddleware, Int } from 'type-graphql'
 import { Post } from '../entities/Post'
+import { Connection, getConnection } from 'typeorm'
 
 @InputType()
 class PostInput {
@@ -15,8 +16,22 @@ class PostInput {
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  async posts(): Promise<Post[]> {
-    return Post.find()
+  async posts(
+    @Arg('limit', () => Int) limit: number,
+    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
+  ): Promise<Post[]> {
+    const realLimit = Math.min(50, limit)
+
+    const qb = getConnection()
+      .getRepository(Post)
+      .createQueryBuilder("p")
+      .orderBy('"createdAt"', 'DESC')
+      .take(realLimit)
+
+    if (cursor) {
+      qb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) })
+    }
+    return qb.getMany()
   }
 
   @Query(() => Post, { nullable: true })
